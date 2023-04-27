d44d9e078e25fec3f2ce6aa0fc57610d64b57336
RuairiGrimes <ruairigrimes924@gmail.com>
2023-04-28 00:14:14 +0100

    Sprint-7-Chnages
    Added style functionality and updated pagination features.
    
    Pokemon detail updated with improved style and working opening/ closing functionality.

--- src/app/pokemon-details/pokemon-details.component.ts
+++ src/app/pokemon-details/pokemon-details.component.ts
@@ -1,17 +1,22 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-pokemon-details',
   templateUrl: './pokemon-details.component.html',
-  styleUrls: ['./pokemon-details.component.css']
+  styleUrls: ['./pokemon-details.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class PokemonDetailsComponent implements OnInit {
   pokemon: any;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, public dialogRef: MatDialogRef<PokemonDetailsComponent>) { }
 
   ngOnInit(): void {
     this.pokemon = this.data; // <-- make sure this is receiving the pokemon object
+  }
+  close(): void {
+    this.dialogRef.close();
   }
 }
