8eac21a6d4801ffc724f83eca8ee0ce2852cedb3
RuairiGrimes <ruairigrimes924@gmail.com>
2023-04-29 17:42:41 +0100

    Commiting Changes for Sprint 1
    Changes Made:
    Pagination Control Fully Functional
    Updated Styles
    API call for Pokemon List Optimized

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { PokemonDetailsComponent } from '../pokemon-details/pokemon-details.component';
 import { DataService } from '../services/data.service';
+import {  PaginationService, PaginationInstance } from 'ngx-pagination';
 
 @Component({
   selector: 'app-pokemon-list',
@@ -16,11 +17,20 @@ export class PokemonListComponent implements OnInit {
   pokemonHover: boolean = false; // Add this property
   itemsPerPage: number = 20; // set items per page to 20
   visiblePokemon: any[] = [];
+  config: PaginationInstance = {
+    id:"px1",
+    itemsPerPage: 20,
+    currentPage: 1,
+    totalItems: 1000
+  };
 
   constructor(
     private dataService: DataService,
-    private dialog: MatDialog
-  ) {}
+    private dialog: MatDialog,
+    private paginationService: PaginationService
+    ) {
+      this.paginationService.register(this.config);
+  }
 
   ngOnInit(): void {
     this.getPokemon();
@@ -28,33 +38,32 @@ export class PokemonListComponent implements OnInit {
 
   uniqueNames = new Set();
   getPokemon() {
-    this.dataService.getPokemon(151, 0)
+    this.dataService.getPokemon(1000, 0)
       .subscribe((response: any) => {
         this.totalPokemon = response.count;
         response.results.forEach((result: any) => {
           this.dataService.getMoreData(result.name)
             .subscribe((uniqueResponse: any) => {
               this.pokemon.push(uniqueResponse);
+              this.config.totalItems = this.pokemon.length;
               this.updateVisiblePokemon();
             });
         });
       });
   }
+
   updateVisiblePokemon() {
-    const startIndex = (this.page - 1) * this.itemsPerPage;
-    const endIndex = startIndex + this.itemsPerPage;
+    const startIndex = (this.config.currentPage - 1) * this.config.itemsPerPage;
+    const endIndex = startIndex + this.config.itemsPerPage;
     this.visiblePokemon = this.pokemon.slice(startIndex, endIndex);
   }
 
-  
   onPageChanged(event: any): void {
     console.log('Page changed:', event);
-    this.page = event.page;
-    const startIndex = (this.page - 1) * this.itemsPerPage;
-    const endIndex = startIndex + this.itemsPerPage;
-    console.log('Visible Pokemon:', this.pokemon.slice(startIndex, endIndex));
-    this.visiblePokemon = this.pokemon.slice(startIndex, endIndex);
+    this.config.currentPage = event;
+    this.updateVisiblePokemon();
   }
+
   // Open dialog
   openDialog(pokemon: any) {
     this.dialogOpen = true;
@@ -65,5 +74,4 @@ export class PokemonListComponent implements OnInit {
       this.dialogOpen = false;
     });
   }
-
 }
