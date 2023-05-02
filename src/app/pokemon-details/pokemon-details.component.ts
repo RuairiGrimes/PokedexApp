7645548968df3485a5cc73f9da9383066e0d291c
RuairiGrimes <ruairigrimes924@gmail.com>
2023-05-02 18:19:30 +0100

    Style Update for Detail Component

--- src/app/pokemon-details/pokemon-details.component.ts
+++ src/app/pokemon-details/pokemon-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { ViewEncapsulation } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-pokemon-details',
@@ -10,13 +11,30 @@ import { ViewEncapsulation } from '@angular/core';
 })
 export class PokemonDetailsComponent implements OnInit {
   pokemon: any;
+  moves!: string[]; // Add this line to declare the 'moves' property
+  showMoves = false;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, public dialogRef: MatDialogRef<PokemonDetailsComponent>) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, public dialogRef: MatDialogRef<PokemonDetailsComponent>, private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.moves = [];
     this.pokemon = this.data; // <-- make sure this is receiving the pokemon object
+    if (this.pokemon && this.pokemon.id) {
+      this.fetchPokemonMoveset(this.pokemon.id);
+    }
   }
   close(): void {
     this.dialogRef.close();
+  }
+  toggleMovesVisibility(): void {
+    this.showMoves = !this.showMoves;
+  }
+  fetchPokemonMoveset(pokemonId: number) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
+    this.http.get(url).subscribe((response: any) => {
+      this.moves = response.moves.map((moveObj: any) => {
+        return moveObj.move.name;
+      });
+    });
   }
 }
