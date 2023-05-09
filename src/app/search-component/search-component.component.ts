6f19f894f8c0d29150486e19fe907876de0c942f
RuairiGrimes <ruairigrimes924@gmail.com>
2023-05-09 14:21:47 +0100

    Generated Search Component

---
+++ src/app/search-component/search-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-search-component',
+  templateUrl: './search-component.component.html',
+  styleUrls: ['./search-component.component.css']
+})
+export class SearchComponentComponent {
+  
+
+}
