9c1d9482902a40350f7b0d8f48a15011ebaca40a
RuairiGrimes <ruairigrimes924@gmail.com>
2023-05-09 12:55:21 +0100

    Fixed Bugs and added API call

--- src/app/pokemon-details/pokemon-details.component.ts
+++ src/app/pokemon-details/pokemon-details.component.ts
@@ -13,6 +13,12 @@ export class PokemonDetailsComponent implements OnInit {
   pokemon: any;
   moves!: string[]; // Add this line to declare the 'moves' property
   showMoves = false;
+  genderRate!: number;
+  malePercentage!: number;
+  femalePercentage!: number;
+  habitat!: string;
+  abilities!: string[];
+  showAdditionalInfo = false;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: any, public dialogRef: MatDialogRef<PokemonDetailsComponent>, private http: HttpClient) { }
 
@@ -21,10 +27,14 @@ export class PokemonDetailsComponent implements OnInit {
     this.pokemon = this.data; // <-- make sure this is receiving the pokemon object
     if (this.pokemon && this.pokemon.id) {
       this.fetchPokemonMoveset(this.pokemon.id);
+      this.fetchPokemonExtraData(this.pokemon.id); 
     }
   }
   close(): void {
     this.dialogRef.close();
+  }
+  toggleAdditionalInfoVisibility(): void {
+    this.showAdditionalInfo = !this.showAdditionalInfo;
   }
   toggleMovesVisibility(): void {
     this.showMoves = !this.showMoves;
@@ -34,6 +44,25 @@ export class PokemonDetailsComponent implements OnInit {
     this.http.get(url).subscribe((response: any) => {
       this.moves = response.moves.map((moveObj: any) => {
         return moveObj.move.name;
+      });
+    });
+  }
+  fetchPokemonExtraData(pokemonId: number) {
+    const speciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${pokemonId}`;
+    const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
+  
+    this.http.get(speciesUrl).subscribe((speciesResponse: any) => {
+      this.genderRate = speciesResponse.gender_rate;
+      this.habitat = speciesResponse.habitat.name;
+  
+      // Calculate gender ratio
+      this.malePercentage = this.genderRate >= 0 ? (this.genderRate / 8) * 100 : -1;
+      this.femalePercentage = this.genderRate >= 0 ? 100 - this.malePercentage : -1;
+  
+      this.http.get(pokemonUrl).subscribe((pokemonResponse: any) => {
+        this.abilities = pokemonResponse.abilities.map((abilityObj: any) => {
+          return abilityObj.ability.name;
+        });
       });
     });
   }
